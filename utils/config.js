be7b6bef56e66746aaaa3998ac44a7121cb469f2
Peter Eck <jpeckiii@gmail.com>
2019-11-07 10:37:49 -0600

    Fix Mongo URI for test env

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
-const MONGO_URI = process.env.MONGO_URI
+let MONGO_URI = process.env.MONGO_URI
 const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === 'test') {
+    MONGO_URI = process.env.TEST_MONGO_URI
+}
 
 module.exports = {
     MONGO_URI,
